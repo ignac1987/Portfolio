f8d7bb7851cb6a6bcabba389a8775cba55803623
ignac1987 <janos.tarcsai@gmail.com>
2025-03-12 19:25:26 +0100

    Kommentar erweitern

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -22,27 +22,29 @@ function Projects() {
 
             </div>
 
-            <div className='flex flex-wrap p-8 justify-evenly items-center'>                
+            <div className='flex flex-wrap p-8 justify-evenly items-center'>
 
-                    {projects.map((project) =>
-                    (
-                        <Tilt key={project.name} className='w-[300px] h-[500px] flex flex-col justify-evenly shadow-2xl m-10 item-center hover:shadow-2xl hover:sadow-blue-400 shadow-blue-300 p-3 rounded-xl border-2 border-blue-900'>
+                {projects.map((project) =>
+                (
+                    <Tilt key={project.name} className='w-[300px] h-[500px] flex flex-col justify-evenly shadow-2xl m-10 item-center hover:shadow-2xl hover:sadow-blue-400 shadow-blue-300 p-3 rounded-xl border-2 border-blue-900'>
 
-                            <div className='flex justify-center items-center mb-3 flex-col'>
+                        <div className='flex h-full justify-center items-center mb-3 flex-col'>
 
-                                <div className='h-[40px] w-[40px] m-3 bg-slate-50 rounded-full shadow-lg shadow-blue-500 flex justify-center items-center'>
+                            <div className='h-[40px] w-[40px] m-3 bg-slate-50 rounded-full shadow-lg shadow-blue-500 flex justify-center items-center'>
 
-                                    <a href={project.source_code_lik} target='blank'>
+                                <a href={project.source_code_lik} target='blank'>
 
-                                        <SiGithub className="text-3xl font-bold text-blue-600"></SiGithub>
+                                    <SiGithub className="text-3xl font-bold text-blue-600"></SiGithub>
 
-                                    </a>
+                                </a>
 
-                                </div>
+                            </div>
 
+                            <div className="flex-1">
+                                <div className="justify-center">
                                 <img src={project.image} alt={project.name} className='h-[200px] w-[250px] self-center border-2 border-blue-400 rounded-2xl' />
-
-                                <div className='text-2xl text-blue-600 font-extrabold'>
+                                </div>
+                                <div className='text-center text-2xl text-blue-600 font-extrabold'>
                                     {project.name}
                                 </div>
 
@@ -50,17 +52,18 @@ function Projects() {
 
                                     <span className='text-blue-500'>
 
-                                        {project.description}                                        
+                                        {project.description}
 
                                     </span>
-                                    
-                                </div>
 
+                                </div>
                             </div>
 
-                        </Tilt>
-                    ))}
-                
+                        </div>
+
+                    </Tilt>
+                ))}
+
             </div>
 
         </div>
